e1b09d40ebf1fc145d46a6adefd0fa86ef376ecb
StupidApe <98391319+Duntss@users.noreply.github.com>
2025-06-17 15:07:26 +0200

    Update find_strange_func.py

--- find_strange_func.py
+++ find_strange_func.py
@@ -1,28 +1,69 @@
-#Find werid call dwords in IDA based on ida_find_func.py
 import idaapi
 import idc
 import idautils
 
+bp = []
+
 def Find_suspicious():
     msg("=== Suspicious CALLs to dword_* ===\n")
     for function in idautils.Functions():
-        # Ignore function from LIB or THUNK
+        # Ignorer les fonctions de bibliothèque ou thunk
         flags = get_func_attr(function, FUNCATTR_FLAGS)
         if flags & FUNC_LIB or flags & FUNC_THUNK:
             continue
 
-        # Goes in every instruction of the function
+        # Parcourir toutes les instructions de la fonction
         for x in idautils.FuncItems(function):
-            # filter for call
+            # Filtrer uniquement les appels
             if idc.print_insn_mnem(x) == "call":
-                # Direct call via memory operand
+            
+                # CALL as memory ref
                 if idc.get_operand_type(x, 0) == o_mem:
                     dest = get_operand_value(x, 0)
                     name = get_name(dest, GN_LONG)
                     if name and name.startswith("dword_"):
-                        msg("0x%08X  ->  %s\n" % (x, name))     
-        
-        
+                        idc.set_color(x,CIC_ITEM,0xA1A600) 
+                        idc.set_color(x,CIC_FUNC,0xB1C813)
+                        msg(hex(x) + " " + idc.generate_disasm_line(x,0)) 
+                        bp.append(x)  
+                
+                #CALL REG
+                elif idc.get_operand_type(x,0) == o_reg:
+                    idc.set_color(x,CIC_ITEM,0xA1A600) 
+                    idc.set_color(x,CIC_FUNC,0xB1C813)
+                    print(hex(x) + " " + idc.generate_disasm_line(x,0))
+                    bp.append(x)
+                    
+            elif idc.print_insn_mnem(x) == "jmp":
+                # JMP as memory ref
+                if idc.get_operand_type(x, 0) == o_mem:
+                    dest = get_operand_value(x, 0)
+                    name = get_name(dest, GN_LONG)
+                    if name and name.startswith("dword_"):
+                        idc.set_color(x,CIC_ITEM,0xA1A600) 
+                        idc.set_color(x,CIC_FUNC,0xB1C813)
+                        msg(hex(x) + " " + idc.generate_disasm_line(x,0))  
+                        bp.append(x)                          
+                
+                #JMP REG
+                elif idc.get_operand_type(x,0) == o_reg:
+                    idc.set_color(x,CIC_ITEM,0xA1A600) 
+                    idc.set_color(x,CIC_FUNC,0xB1C813)
+                    print(hex(x) + " " + idc.generate_disasm_line(x,0)) 
+                    bp.append(x) 
+                    
+            elif idc.print_insn_mnem(x) == "cpuid":
+                    idc.set_color(x,CIC_ITEM,0xA1A600) 
+                    idc.set_color(x,CIC_FUNC,0xB1C813)
+                    print(hex(x) + " " + idc.generate_disasm_line(x,0)) 
+                    #bp.append(x) 
+                    
+            elif idc.print_insn_mnem(x) == "rdtsc":
+                    idc.set_color(x,CIC_ITEM,0xA1A600) 
+                    idc.set_color(x,CIC_FUNC,0xB1C813)
+                    print(hex(x) + " " + idc.generate_disasm_line(x,0)) 
+                    #bp.append(x) 
+                            
     
 def FindUnknownFunctions():
     print("Looking for unknown functions – passe 1")
@@ -30,21 +71,29 @@ def FindUnknownFunctions():
     start = seg.start_ea
     BAD = idaapi.BADADDR
 
+    # Passe 1 : on crée la fonction dès qu'on trouve un trou
     ea = idaapi.find_not_func(start, idaapi.SEARCH_DOWN)
     while ea != BAD:
         idaapi.add_func(ea)
         ea = idaapi.find_not_func(ea + 1, idaapi.SEARCH_DOWN)
 
     print("Looking for unknown functions – passe 2")
+    # Passe 2 : on redéfinit la fin de chaque fonction
     ea = idaapi.find_not_func(start, idaapi.SEARCH_DOWN)
     while ea != BAD:
+        # cherche la prochaine donnée ou la fin de section
         end_ea = idaapi.find_data(ea, idaapi.SEARCH_DOWN)
         if end_ea == BAD or end_ea > seg.end_ea:
             end_ea = seg.end_ea
         idaapi.add_func(ea, end_ea)
         ea = idaapi.find_not_func(ea + 1, idaapi.SEARCH_DOWN)
 
-    print("Done!")
+    
 
 FindUnknownFunctions()
 Find_suspicious()
+print("\n\n---------------------------------- start xdbg script ---------------------------------- \n\n")
+for mybp in bp:
+    print("bp "+ hex(mybp))
+print("---------------------------------- end xdbg script ----------------------------------")
+print("Done!")
